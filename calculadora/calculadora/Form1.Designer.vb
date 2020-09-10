eea7b9d7127bfd27dc1acfb15ac7740161fedd3c
CesiaFlores09 <florescsia@gmail.com>
2020-09-09 19:54:43 -0600

    Trabajo con matrices y POO, calcular media aritmetica, varianza, y desviacion tipica

--- calculadora/calculadora/Form1.Designer.vb
+++ calculadora/calculadora/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,198 +20,125 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblNum1 = New System.Windows.Forms.Label()
-        Me.lblNum2 = New System.Windows.Forms.Label()
-        Me.txtNum1 = New System.Windows.Forms.TextBox()
-        Me.txtNum2 = New System.Windows.Forms.TextBox()
-        Me.optSuma = New System.Windows.Forms.RadioButton()
-        Me.optResta = New System.Windows.Forms.RadioButton()
-        Me.optMul = New System.Windows.Forms.RadioButton()
-        Me.optDivsion = New System.Windows.Forms.RadioButton()
-        Me.optPorcentaje = New System.Windows.Forms.RadioButton()
-        Me.optResiduo = New System.Windows.Forms.RadioButton()
-        Me.optExponenciacion = New System.Windows.Forms.RadioButton()
-        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.txtSerie = New System.Windows.Forms.TextBox()
+        Me.lblSerie = New System.Windows.Forms.Label()
         Me.lblRespuesta = New System.Windows.Forms.Label()
-        Me.cboOperaciones = New System.Windows.Forms.ComboBox()
+        Me.btnCalculosE = New System.Windows.Forms.Button()
+        Me.lblMedia = New System.Windows.Forms.Label()
+        Me.lblVarianza = New System.Windows.Forms.Label()
+        Me.LblResV = New System.Windows.Forms.Label()
+        Me.lblDesviacionT = New System.Windows.Forms.Label()
+        Me.lblResDesTipica = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'lblNum1
+        'txtSerie
         '
-        Me.lblNum1.AutoSize = True
-        Me.lblNum1.Location = New System.Drawing.Point(38, 83)
-        Me.lblNum1.Name = "lblNum1"
-        Me.lblNum1.Size = New System.Drawing.Size(53, 13)
-        Me.lblNum1.TabIndex = 0
-        Me.lblNum1.Text = "Numero 1"
+        Me.txtSerie.Location = New System.Drawing.Point(66, 58)
+        Me.txtSerie.Name = "txtSerie"
+        Me.txtSerie.Size = New System.Drawing.Size(253, 20)
+        Me.txtSerie.TabIndex = 1
         '
-        'lblNum2
+        'lblSerie
         '
-        Me.lblNum2.AutoSize = True
-        Me.lblNum2.Location = New System.Drawing.Point(419, 87)
-        Me.lblNum2.Name = "lblNum2"
-        Me.lblNum2.Size = New System.Drawing.Size(53, 13)
-        Me.lblNum2.TabIndex = 1
-        Me.lblNum2.Text = "Numero 2"
-        '
-        'txtNum1
-        '
-        Me.txtNum1.Location = New System.Drawing.Point(112, 83)
-        Me.txtNum1.Name = "txtNum1"
-        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
-        Me.txtNum1.TabIndex = 2
-        '
-        'txtNum2
-        '
-        Me.txtNum2.Location = New System.Drawing.Point(526, 80)
-        Me.txtNum2.Name = "txtNum2"
-        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
-        Me.txtNum2.TabIndex = 3
-        '
-        'optSuma
-        '
-        Me.optSuma.AutoSize = True
-        Me.optSuma.Location = New System.Drawing.Point(293, 31)
-        Me.optSuma.Name = "optSuma"
-        Me.optSuma.Size = New System.Drawing.Size(31, 17)
-        Me.optSuma.TabIndex = 4
-        Me.optSuma.TabStop = True
-        Me.optSuma.Text = "+"
-        Me.optSuma.UseVisualStyleBackColor = True
-        '
-        'optResta
-        '
-        Me.optResta.AutoSize = True
-        Me.optResta.Location = New System.Drawing.Point(293, 81)
-        Me.optResta.Name = "optResta"
-        Me.optResta.Size = New System.Drawing.Size(28, 17)
-        Me.optResta.TabIndex = 5
-        Me.optResta.TabStop = True
-        Me.optResta.Text = "-"
-        Me.optResta.UseVisualStyleBackColor = True
-        '
-        'optMul
-        '
-        Me.optMul.AutoSize = True
-        Me.optMul.Location = New System.Drawing.Point(293, 125)
-        Me.optMul.Name = "optMul"
-        Me.optMul.Size = New System.Drawing.Size(29, 17)
-        Me.optMul.TabIndex = 6
-        Me.optMul.TabStop = True
-        Me.optMul.Text = "*"
-        Me.optMul.UseVisualStyleBackColor = True
-        '
-        'optDivsion
-        '
-        Me.optDivsion.AutoSize = True
-        Me.optDivsion.Location = New System.Drawing.Point(293, 167)
-        Me.optDivsion.Name = "optDivsion"
-        Me.optDivsion.Size = New System.Drawing.Size(30, 17)
-        Me.optDivsion.TabIndex = 7
-        Me.optDivsion.TabStop = True
-        Me.optDivsion.Text = "/"
-        Me.optDivsion.UseVisualStyleBackColor = True
-        '
-        'optPorcentaje
-        '
-        Me.optPorcentaje.AutoSize = True
-        Me.optPorcentaje.Location = New System.Drawing.Point(293, 203)
-        Me.optPorcentaje.Name = "optPorcentaje"
-        Me.optPorcentaje.Size = New System.Drawing.Size(76, 17)
-        Me.optPorcentaje.TabIndex = 8
-        Me.optPorcentaje.TabStop = True
-        Me.optPorcentaje.Text = "Porcentaje"
-        Me.optPorcentaje.UseVisualStyleBackColor = True
-        '
-        'optResiduo
-        '
-        Me.optResiduo.AutoSize = True
-        Me.optResiduo.Location = New System.Drawing.Point(293, 240)
-        Me.optResiduo.Name = "optResiduo"
-        Me.optResiduo.Size = New System.Drawing.Size(64, 17)
-        Me.optResiduo.TabIndex = 9
-        Me.optResiduo.TabStop = True
-        Me.optResiduo.Text = "Residuo"
-        Me.optResiduo.UseVisualStyleBackColor = True
-        '
-        'optExponenciacion
-        '
-        Me.optExponenciacion.AutoSize = True
-        Me.optExponenciacion.Location = New System.Drawing.Point(293, 279)
-        Me.optExponenciacion.Name = "optExponenciacion"
-        Me.optExponenciacion.Size = New System.Drawing.Size(31, 17)
-        Me.optExponenciacion.TabIndex = 10
-        Me.optExponenciacion.TabStop = True
-        Me.optExponenciacion.Text = "^"
-        Me.optExponenciacion.UseVisualStyleBackColor = True
-        '
-        'btnCalcular
-        '
-        Me.btnCalcular.Location = New System.Drawing.Point(112, 222)
-        Me.btnCalcular.Name = "btnCalcular"
-        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
-        Me.btnCalcular.TabIndex = 11
-        Me.btnCalcular.Text = "Calcular"
-        Me.btnCalcular.UseVisualStyleBackColor = True
+        Me.lblSerie.AutoSize = True
+        Me.lblSerie.Location = New System.Drawing.Point(26, 61)
+        Me.lblSerie.Name = "lblSerie"
+        Me.lblSerie.Size = New System.Drawing.Size(34, 13)
+        Me.lblSerie.TabIndex = 2
+        Me.lblSerie.Text = "Serie:"
         '
         'lblRespuesta
         '
         Me.lblRespuesta.AutoSize = True
-        Me.lblRespuesta.Location = New System.Drawing.Point(432, 222)
+        Me.lblRespuesta.Location = New System.Drawing.Point(143, 108)
         Me.lblRespuesta.Name = "lblRespuesta"
-        Me.lblRespuesta.Size = New System.Drawing.Size(64, 13)
-        Me.lblRespuesta.TabIndex = 12
-        Me.lblRespuesta.Text = "Respuesta?"
+        Me.lblRespuesta.Size = New System.Drawing.Size(13, 13)
+        Me.lblRespuesta.TabIndex = 3
+        Me.lblRespuesta.Text = "?"
         '
-        'cboOperaciones
+        'btnCalculosE
         '
-        Me.cboOperaciones.FormattingEnabled = True
-        Me.cboOperaciones.Items.AddRange(New Object() {"Seleccione una opcion", "Suma", "Resta", "Multiplicacion ", "Division"})
-        Me.cboOperaciones.Location = New System.Drawing.Point(505, 152)
-        Me.cboOperaciones.Name = "cboOperaciones"
-        Me.cboOperaciones.Size = New System.Drawing.Size(121, 21)
-        Me.cboOperaciones.TabIndex = 13
+        Me.btnCalculosE.Location = New System.Drawing.Point(66, 218)
+        Me.btnCalculosE.Name = "btnCalculosE"
+        Me.btnCalculosE.Size = New System.Drawing.Size(124, 39)
+        Me.btnCalculosE.TabIndex = 4
+        Me.btnCalculosE.Text = "Calculos Estadisticos"
+        Me.btnCalculosE.UseVisualStyleBackColor = True
+        '
+        'lblMedia
+        '
+        Me.lblMedia.AutoSize = True
+        Me.lblMedia.Location = New System.Drawing.Point(26, 108)
+        Me.lblMedia.Name = "lblMedia"
+        Me.lblMedia.Size = New System.Drawing.Size(39, 13)
+        Me.lblMedia.TabIndex = 5
+        Me.lblMedia.Text = "Media:"
+        '
+        'lblVarianza
+        '
+        Me.lblVarianza.AutoSize = True
+        Me.lblVarianza.Location = New System.Drawing.Point(26, 138)
+        Me.lblVarianza.Name = "lblVarianza"
+        Me.lblVarianza.Size = New System.Drawing.Size(51, 13)
+        Me.lblVarianza.TabIndex = 6
+        Me.lblVarianza.Text = "Varianza:"
+        '
+        'LblResV
+        '
+        Me.LblResV.AutoSize = True
+        Me.LblResV.Location = New System.Drawing.Point(143, 138)
+        Me.LblResV.Name = "LblResV"
+        Me.LblResV.Size = New System.Drawing.Size(13, 13)
+        Me.LblResV.TabIndex = 7
+        Me.LblResV.Text = "?"
+        '
+        'lblDesviacionT
+        '
+        Me.lblDesviacionT.AutoSize = True
+        Me.lblDesviacionT.Location = New System.Drawing.Point(29, 172)
+        Me.lblDesviacionT.Name = "lblDesviacionT"
+        Me.lblDesviacionT.Size = New System.Drawing.Size(95, 13)
+        Me.lblDesviacionT.TabIndex = 8
+        Me.lblDesviacionT.Text = "Desviacion Tipica:"
+        '
+        'lblResDesTipica
+        '
+        Me.lblResDesTipica.AutoSize = True
+        Me.lblResDesTipica.Location = New System.Drawing.Point(143, 172)
+        Me.lblResDesTipica.Name = "lblResDesTipica"
+        Me.lblResDesTipica.Size = New System.Drawing.Size(13, 13)
+        Me.lblResDesTipica.TabIndex = 9
+        Me.lblResDesTipica.Text = "?"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.cboOperaciones)
+        Me.ClientSize = New System.Drawing.Size(342, 347)
+        Me.Controls.Add(Me.lblResDesTipica)
+        Me.Controls.Add(Me.lblDesviacionT)
+        Me.Controls.Add(Me.LblResV)
+        Me.Controls.Add(Me.lblVarianza)
+        Me.Controls.Add(Me.lblMedia)
+        Me.Controls.Add(Me.btnCalculosE)
         Me.Controls.Add(Me.lblRespuesta)
-        Me.Controls.Add(Me.btnCalcular)
-        Me.Controls.Add(Me.optExponenciacion)
-        Me.Controls.Add(Me.optResiduo)
-        Me.Controls.Add(Me.optPorcentaje)
-        Me.Controls.Add(Me.optDivsion)
-        Me.Controls.Add(Me.optMul)
-        Me.Controls.Add(Me.optResta)
-        Me.Controls.Add(Me.optSuma)
-        Me.Controls.Add(Me.txtNum2)
-        Me.Controls.Add(Me.txtNum1)
-        Me.Controls.Add(Me.lblNum2)
-        Me.Controls.Add(Me.lblNum1)
+        Me.Controls.Add(Me.lblSerie)
+        Me.Controls.Add(Me.txtSerie)
         Me.Name = "Form1"
-        Me.Text = "Calculadora"
+        Me.Text = "Matrices Y POO"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents lblNum1 As Label
-    Friend WithEvents lblNum2 As Label
-    Friend WithEvents txtNum1 As TextBox
-    Friend WithEvents txtNum2 As TextBox
-    Friend WithEvents optSuma As RadioButton
-    Friend WithEvents optResta As RadioButton
-    Friend WithEvents optMul As RadioButton
-    Friend WithEvents optDivsion As RadioButton
-    Friend WithEvents optPorcentaje As RadioButton
-    Friend WithEvents optResiduo As RadioButton
-    Friend WithEvents optExponenciacion As RadioButton
-    Friend WithEvents btnCalcular As Button
+    Friend WithEvents txtSerie As TextBox
+    Friend WithEvents lblSerie As Label
     Friend WithEvents lblRespuesta As Label
-    Friend WithEvents cboOperaciones As ComboBox
+    Friend WithEvents btnCalculosE As Button
+    Friend WithEvents lblMedia As Label
+    Friend WithEvents lblVarianza As Label
+    Friend WithEvents LblResV As Label
+    Friend WithEvents lblDesviacionT As Label
+    Friend WithEvents lblResDesTipica As Label
 End Class
--- calculadora/calculadora/Form1.vb
+++ calculadora/calculadora/Form1.vb
@@ -1,45 +1,10 @@
 ﻿Public Class Form1
-    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
-        Dim num1, num2 As Double
-        num1 = txtNum1.Text
-        num2 = txtNum2.Text
+    Dim objEstadistica As New estadistica
 
 
-        If optSuma.Checked Then
-            lblRespuesta.Text = num1 + num2
-        End If
-        If optResta.Checked Then
-            lblRespuesta.Text = num1 - num2
-        End If
-        If optMul.Checked Then
-            lblRespuesta.Text = num1 * num2
-        End If
-        If optDivsion.Checked Then
-            lblRespuesta.Text = num1 / num2
-        End If
-        If optExponenciacion.Checked Then
-            lblRespuesta.Text = num1 ^ num2
-        End If
-        If optResiduo.Checked Then
-            lblRespuesta.Text = num1 Mod num2
-        End If
-        If optPorcentaje.Checked Then
-            lblRespuesta.Text = num2 * num1 / 100
-        End If
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 1 'Suma
-                lblRespuesta.Text = num1 + num2
-            Case 2 ' Resta
-                lblRespuesta.Text = num1 - num2
-            Case 3 'Multiplicacion
-                lblRespuesta.Text = num1 * num2
-            Case 4 ' Division
-                lblRespuesta.Text = num1 / num2
-        End Select
-
+    Private Sub btnCalculosE_Click(sender As Object, e As EventArgs) Handles btnCalculosE.Click
+        lblRespuesta.Text = objEstadistica.calcularMedia(txtSerie.Text.Split(","))
+        LblResV.Text = objEstadistica.calcularVarianza(txtSerie.Text.Split(","))
+        lblResDesTipica.Text = objEstadistica.calcularDesviacionTipica(txtSerie.Text.Split(","))
     End Sub
-
-
-
 End Class
