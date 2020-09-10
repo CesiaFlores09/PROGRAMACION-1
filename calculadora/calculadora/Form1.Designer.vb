3d0eef379a9d2d47e02d12bb7a20846a5a659a79
CesiaFlores09 <florescsia@gmail.com>
2020-09-10 16:33:35 -0600

    Se agrego cuatro departamentos y area de un municipio

--- calculadora/calculadora/Form1.Designer.vb
+++ calculadora/calculadora/Form1.Designer.vb
@@ -26,12 +26,14 @@ Partial Class Form1
         Me.lblDepto = New System.Windows.Forms.Label()
         Me.cboDepto = New System.Windows.Forms.ComboBox()
         Me.cboMun = New System.Windows.Forms.ComboBox()
+        Me.lblArea = New System.Windows.Forms.Label()
+        Me.cboArea = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
         'lblIMun
         '
         Me.lblIMun.AutoSize = True
-        Me.lblIMun.Location = New System.Drawing.Point(218, 24)
+        Me.lblIMun.Location = New System.Drawing.Point(209, 39)
         Me.lblIMun.Name = "lblIMun"
         Me.lblIMun.Size = New System.Drawing.Size(28, 13)
         Me.lblIMun.TabIndex = 7
@@ -40,7 +42,7 @@ Partial Class Form1
         'lblDepto
         '
         Me.lblDepto.AutoSize = True
-        Me.lblDepto.Location = New System.Drawing.Point(21, 24)
+        Me.lblDepto.Location = New System.Drawing.Point(12, 39)
         Me.lblDepto.Name = "lblDepto"
         Me.lblDepto.Size = New System.Drawing.Size(36, 13)
         Me.lblDepto.TabIndex = 0
@@ -49,7 +51,7 @@ Partial Class Form1
         'cboDepto
         '
         Me.cboDepto.FormattingEnabled = True
-        Me.cboDepto.Location = New System.Drawing.Point(64, 15)
+        Me.cboDepto.Location = New System.Drawing.Point(63, 36)
         Me.cboDepto.Name = "cboDepto"
         Me.cboDepto.Size = New System.Drawing.Size(121, 21)
         Me.cboDepto.TabIndex = 8
@@ -57,16 +59,35 @@ Partial Class Form1
         'cboMun
         '
         Me.cboMun.FormattingEnabled = True
-        Me.cboMun.Location = New System.Drawing.Point(252, 21)
+        Me.cboMun.Location = New System.Drawing.Point(243, 36)
         Me.cboMun.Name = "cboMun"
         Me.cboMun.Size = New System.Drawing.Size(121, 21)
         Me.cboMun.TabIndex = 9
+        '
+        'lblArea
+        '
+        Me.lblArea.AutoSize = True
+        Me.lblArea.Location = New System.Drawing.Point(398, 44)
+        Me.lblArea.Name = "lblArea"
+        Me.lblArea.Size = New System.Drawing.Size(29, 13)
+        Me.lblArea.TabIndex = 10
+        Me.lblArea.Text = "Area"
+        '
+        'cboArea
+        '
+        Me.cboArea.FormattingEnabled = True
+        Me.cboArea.Location = New System.Drawing.Point(451, 36)
+        Me.cboArea.Name = "cboArea"
+        Me.cboArea.Size = New System.Drawing.Size(121, 21)
+        Me.cboArea.TabIndex = 11
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(501, 218)
+        Me.ClientSize = New System.Drawing.Size(668, 218)
+        Me.Controls.Add(Me.cboArea)
+        Me.Controls.Add(Me.lblArea)
         Me.Controls.Add(Me.cboMun)
         Me.Controls.Add(Me.cboDepto)
         Me.Controls.Add(Me.lblIMun)
@@ -81,4 +102,6 @@ Partial Class Form1
     Friend WithEvents lblDepto As Label
     Friend WithEvents cboDepto As ComboBox
     Friend WithEvents cboMun As ComboBox
+    Friend WithEvents lblArea As Label
+    Friend WithEvents cboArea As ComboBox
 End Class
--- calculadora/calculadora/Form1.vb
+++ calculadora/calculadora/Form1.vb
@@ -1,5 +1,8 @@
 ﻿Public Class Form1
+
     Dim objdirecciones = New direcciones
+
+
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         cboDepto.Items.AddRange(objdirecciones.depto)
@@ -9,16 +12,41 @@
         cboMun.Items.Clear()
         cboMun.Text = ""
         cboMun.Items.AddRange(objdirecciones.mun(cboDepto.SelectedIndex))
+
     End Sub
+
+    Private Sub cboMun_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboMun.SelectedIndexChanged
+        cboArea.Items.Clear()
+        cboArea.Text = ""
+        cboArea.Items.AddRange(objdirecciones.area(cboMun.SelectedIndex))
+    End Sub
+
 End Class
 Class direcciones
-    Public depto() As String = {"Seleccione un depto", "Usulutan", "San Miguel", "La union ", "Morazan"}
+    Public depto() As String = {"Seleccione un depto", "Usulutan", "San Miguel", "La union ", "Morazan", "La Paz", "San Salvador", "La libertad", "Sonsonate"}
+
+    Public area()() As String = {
+        New String() {"Seleccione un mun"},
+        New String() {"Seleccione una area", "Buena Vista", "El Cerrito", "Santa Barbara", "La Presa"},
+        New String() {" "},
+        New String() {" "},
+        New String() {" "},
+        New String() {" "},
+        New String() {" "},
+        New String() {" "},
+        New String() {" "},
+        New String() {" "}
+        }
     Public mun()() As String = {
         New String() {"Seleccione un depto"},
-         New String() {"Seleccioen un Mun", "Usulutan", "Jiquilisco", "Santa Elena", "Santa Maria"},'Uaulutan
-         New String() {"Seleccioen un Mun", "San Miguel", "El Transito", "San Rafael Ote", "San Jorgue"},'San Miguel
-         New String() {"Seleccioen un Mun", "La Union", "SRl", "Anamoros", "Bolivar"},'La Union
-        New String() {"Seleccioen un Mun", "Arambala", "Perquin", "Corinto", "Cacaopera"}'Morazan 
+        New String() {"Seleccioen un Mun", "Usulutan", "Jiquilisco", "Santa Elena", "Santa Maria"},'Usulutan
+        New String() {"Seleccioen un Mun", "San Miguel", "El Transito", "San Rafael Ote", "San Jorgue"},'San Miguel
+        New String() {"Seleccioen un Mun", "La Union", "SRl", "Anamoros", "Bolivar"},'La Union
+        New String() {"Seleccioen un Mun", "Arambala", "Perquin", "Corinto", "Cacaopera"}, 'Morazan 
+        New String() {"Seleccioen un Mun", "Zacatecoluca", "El Rosario", "Santiago Nonualco", "Olocuilta"}, 'La Paz
+        New String() {"Seleccioen un Mun", "San Salvador", "Aguilares", "Apopa", "Ilopango"},'San Salvador
+        New String() {"Seleccioen un Mun", "Santa Tecla", "Ciudad Arce", "Colon", "La Libertad"}, 'La Libertad
+        New String() {"Seleccioen un Mun", "Sonsonate", "Acajjutla", "Izalco", "Juayua"} 'San Salvador
     }
 
 
