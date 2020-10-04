7588af5a26c5cf44662f246cb21ca10f0b421639
CesiaFlores09 <florescsia@gmail.com>
2020-10-03 19:23:37 -0600

    Creacion de la BD db_sistema y creacion de la tabla clientes practica de comandos SQL

---
+++ calculadora/calculadora/comandos.sql
@@ -0,0 +1,7 @@
+﻿
+select  nombre, email from Clientes where email like '%com%'
+select * from Clientes
+insert into Clientes(codigo,nombre,direccion,telefono,email) values ( '004', 'Adonay Orellana','Berlin','2245-2345','adonay@gmail.com')
+delete from Clientes where IdClientes=1
+update Clientes set codigo ='0002', nombre=	'Monica Rodriguez', telefono ='5643-0987', email='Monicar@gmail.com' where IdClientes=2
+update Clientes set codigo='002' where IdClientes=2
